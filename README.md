bc1c0067fb5c5f837ccb04138413a87667da79c7
ITI <private.kosinsky@gmail.com>
2024-01-31 23:45:53 +0100

    everything works

--- README.md
+++ README.md
@@ -6,27 +6,37 @@ En|CO|De is API for encryption and decoding images(steganography), builded on th
     1. Clone this repository
     2. Create virtual environment
     3. Install requirements.txt
-    4. In main.py, line 136 remove app.run(debug=True) and uncomment app.run()
     4. Run main.py
     5. Open in browser http://
 
 
-## Usage
-    Write data for your SMTP in connection line in main.py(lines: 19-30, 114-131)
-    Remove in the end of main.py debug=True
-    You always can rewrite your own logic for encryption and decode into the file steganography.py
-    It use Bootstrap v5.3 connection line and classes
+## Used libraries
+    blinker==1.7.0
+    cffi==1.16.0
+    click==8.1.7
+    cryptography==42.0.0
+    Flask==3.0.1
+    Flask-Mail==0.9.1
+    itsdangerous==2.1.2
+    Jinja2==3.1.3
+    MarkupSafe==2.1.4
+    pillow==10.2.0
+    pycparser==2.21
+    Werkzeug==3.0.1
 
 ## Usage for MAIL
-    In main.py uncomment lines 26-30 and write your own data for SMTP
+    Write data for your SMTP in connection line in main.py(lines: 21-30, 152-171)
+    You always can rewrite your own logic for encryption and decode into the file steganography.py
 
 ![uncomment](screenshots/image-2.png "uncomment")
 
 ![uncomment](screenshots/image-1.png "uncomment")
-    
     in templates/encrypted.html 
 ![uncomment](screenshots/image-3.png "uncomment")
     and upper in the same file in line 22 change description for your own
 
 ## Todo
+    add js(ts)
     rewrite for vite front with react
+    more design
+    
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, render_template, redirect, send_file, url_for,session
-from flask_mail import Mail, Message
-import steganography 
+#TODO uncommnet for mail 
+# from flask_mail import Mail, Message
+# import webbrowser as web_br
+from PIL import Image
+from steganography import Steganography
 import secrets
 import string
 import os
@@ -17,7 +20,7 @@ app = Flask(__name__)
 app.secret_key = generate_random_key(32)
 
 # !Config for mail
-#todo uncomment this for config mail
+#TODO uncomment this for config mail
 # app.config['MAIL_SERVER'] = ''  # Replace with your SMTP server
 # app.config['MAIL_PORT'] = 0  # Replace with the SMTP port
 # app.config['MAIL_USE_TLS'] = True
@@ -31,14 +34,21 @@ app.secret_key = generate_random_key(32)
 def index():
     return render_template("index.html")
 
-#download file
+#download image
 @app.route('/download/<path:filename>', methods=['POST'])
 def download(filename):
     if request.method == 'POST' and session.get('_imgname')==filename:
         return send_file('encrypted/'+filename, as_attachment=True, download_name=filename)
     else:
         return render_template('error.html', error = 'No file for downloading' )
-
+#download keys
+@app.route('/download-keys/<path:filename>', methods=['POST'])
+def download_keys(filename):
+    if request.method == 'POST' and session.get('_imgname')==filename:
+        filename=filename.replace(".png", "")+".txt"
+        return send_file('textfiles/'+filename, as_attachment=True, download_name=filename)
+    else:
+        return render_template('error.html', error = 'No file for downloading' )
 #upload file
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -52,14 +62,16 @@ def upload_file():
     
     if file.filename == '':
         return render_template('error.html', error = 'No selected file')
-    # if file.filename.endswith('.txt'):     enother one method for one type check
-    # if file and allowed_file(file.filename):
     # Process the file upload
     file.save('uploads/' + file.filename)
-        # return 'File uploaded successfully'
+    #check file type
     if os.path.splitext(file.filename)[1] != ".png":
         os.remove('uploads/' + file.filename)
         return render_template('error.html', error = 'Invalid file format')
+    #check file size
+    if Image.open('uploads/' + file.filename).size[0] <= 1000 or Image.open('uploads/' + file.filename).size[1] <= 1000:
+        return render_template('error.html', error = 'Image size must be more than 1000x1000')
+    #set name of file to session
     session['_imgname'] = file.filename
     # redirects from radiobuttons
     if process == "encr":
@@ -70,56 +82,82 @@ def upload_file():
 #process of decoding
 @app.route("/decode",methods=["GET","POST"])
 def decode():
-    if request.method == 'POST':
-        password = request.form.get('password')
-        if password is None or password.strip() == "":
+    if request.method == 'POST' and session.get('_imgname') is not None:
+        key = request.form.get('key')
+        userPassword = request.form.get('password')
+        if key is None or key.strip() == "":
+            return render_template('error.html', error = "Some problem with your key")
+        elif userPassword is None or userPassword.strip() == "":
             return render_template('error.html', error =  "Some problem with your password")
         else:
             session['_password'] = request.form.get("password")
             session['_key'] = request.form.get("key")
             return redirect(url_for('decoded'))
     else:
+        if session.get('_imgname') is None:
+            return render_template('error.html', error = "You haven`t uploaded the image")
         return render_template('decode.html')
 
 #result of decoding process 
 @app.route("/decoded",methods=['GET'])
 def decoded():
+    if session.get('_imgname') is None:
+        return render_template('error.html', error = "You haven`t uploaded the image")
     fileName = session.get('_imgname')
-    password = session.get('_password')
-    key = session.get('_key')
-    
-    text = steganography.decode(("uploads/"+fileName),password,key)
+    userPassword = session.get('_password')
+    userKey = session.get('_key')
+
+    try:
+        userText = Steganography.decode(("uploads/"+fileName),stringOfKeys=userKey,password=userPassword)
+    except:
+        return render_template('error.html', error = "Something is wrong with your key or password. Try again")
+    # remove uploaded file
     os.remove(("uploads/"+fileName))
-    return render_template('decoded.html',decoded_phrase=text)
+    return render_template('decoded.html',decoded_phrase=userText)
 
 #process of encryption
 @app.route('/encrypt',methods=["GET","POST"])
 def encrypt():
-    if request.method == "POST":  
-        text = request.form.get('text')
-        if text is None or text.strip() == "":
+    if request.method == "POST" and session.get('_imgname') is not None:  
+        userText = request.form.get('text')
+        userPassword = request.form.get('password')
+        if userText is None or userText.strip() == "":
             return render_template('error.html', error = "You haven`t written the text")
+        elif userPassword is None or userPassword.strip() == "":
+            return render_template('error.html', error = "You haven`t written the password")
         else:
             session['_text'] = request.form.get("text")
+            session['_password'] = request.form.get("password")
             return redirect(url_for("encrypted"))    
     else:
+        if session.get('_imgname') is None:
+            return render_template('error.html', error = "You haven`t uploaded the image")
         return render_template('encrypt.html')
       
 #result of encryption process
 @app.route('/encrypted',methods=['GET'])
 def encrypted():
-    name = session.get('_imgname')
-    text = session.get('_text')
-    password, key = steganography.encryption(('uploads/'+str(name)),text,name,'encrypted/')
-    return render_template('encrypted.html',ret_paswd=password, name=name, key=key)
+    if session.get('_imgname') is None:
+        return render_template('error.html', error = "You haven`t uploaded the image")
+    userPassword = session.get('_password')
+    userText = session.get('_text')
+    imageName = session.get('_imgname')
+    try:
+        userPassword = Steganography.encryption(('uploads/'+str(imageName)),userText,'encrypted/',userPassword)
+    except:
+        return render_template('error.html', error = "Something is wrong with your image. Try again with another image")
+    
+        
+    return render_template('encrypted.html',ret_paswd=userPassword, name=imageName)
+#TODO uncomment this for config mail
 #!===================== uncomment this for config mail
 # @app.route('/email', methods=["POST"])
 # def sendMail():
-#     if request.method == 'POST':
-#         name = session.get('_imgname')
+#     if request.method == 'POST' and session.get('_imgname') is not None:
+#         imageName = session.get('_imgname')
 #         email = request.form['email']
 #         # File path
-#         filename = 'uploads/'+ name
+#         filename = 'uploads/'+ imageName
 #                  #!add sender ""
 #         msg = Message('Sending File', sender='', recipients=[email])
 #         msg.body = 'Your encrypted image'
@@ -127,11 +165,11 @@ def encrypted():
 #             msg.attach(filename, 'application/octet-stream', fp.read())
 
 #         mail.send(msg)
-#         os.remove(filename)
-#         return render_template('end.html', result ='Email sent successfully')
+#         # remove file after sending
+#         # os.remove(filename)
+#         return web_br.open_new_tab('end.html', result ='Email sent successfully')
 # !=====================
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run()
+    app.run()
 
